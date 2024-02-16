7597a1e409e992cdade75cf71a9950a5af960c34
albertofp <albertopluecker@gmail.com>
2024-02-16 20:50:19 +0100

    error handling tp1.1

--- python1/tp1/questao_1.py
+++ python1/tp1/questao_1.py
@@ -5,12 +5,14 @@ def process_numbers(x: np.double, y: np.double) -> None:
     print(f"{x} + {y} = {x + y}")
     print(f"{x} - {y} = {x - y}")
     print(f"{x} * {y} = {x * y}")
-    print(f"{x} / {y} = {x / y}")
+    try:
+        print(f"{x} / {y} = {x / y}")
+    except ZeroDivisionError:
+        print(f"Divisão por zero!")
 
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(
         description=
         "Recebe dois números e calcula o resultado das 4 operações aritmeticas: adição, subtração, divisão e multiplicação"
@@ -19,5 +21,8 @@ if __name__ == "__main__":
     parser.add_argument("y", help="Segundo valor", type=np.double)
 
     args = parser.parse_args()
+
+    if args.y == 0:
+        parser.error("Segundo valor deve ser diferente de zero")
 
     process_numbers(args.x, args.y)
